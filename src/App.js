5406f220b3d870601faf6993f2547dc0008bdf3d
vituchini <69416374+vituchini@users.noreply.github.com>
2022-09-25 10:07:23 +0300

    Make components receive dynamic values (#3)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
 
+import pizzas from './assets/pizzas.json'
+
 import './scss/app.scss'
 
 function App() {
@@ -17,7 +19,9 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            <PizzaBlock title='Мексиканская' price={500} />
+            {pizzas.map((pizza) => (
+              <PizzaBlock {...pizza} />
+            ))}
           </div>
         </div>
       </div>
