4cebfae6a9fb3200059bcbc8ff0d8e1e7e7f92bf
vituchini <viktor.petrovych.work@gmail.com>
2022-09-26 20:00:02 +0300

    Fix sort layout & make sort from API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
           <div className='header__logo'>
             <img width='38' src={logo} alt='Pizza logo' />
             <div>
-              <h1>React Pizza</h1>
+              <h1>React Pizza V2</h1>
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,22 @@ import Skeleton from '../components/PizzaBlock/Skeleton'
 const Home = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [categoryId, setCategoryId] = useState(0)
+  const [sortType, setSortType] = useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  })
 
   useEffect(() => {
-    fetch('https://628ba2c37886bbbb37bc9a31.mockapi.io/items')
+    setIsLoading(true)
+
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const sortBy = sortType.sortProperty.replace('-', '')
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+
+    fetch(
+      `https://628ba2c37886bbbb37bc9a31.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
       .then((res) => {
         return res.json()
       })
@@ -18,13 +31,13 @@ const Home = () => {
         setIsLoading(false)
       })
     window.scrollTo(0, 0)
-  }, [])
+  }, [categoryId, sortType])
 
   return (
     <div className='container'>
       <div className='content__top'>
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onClickSort={(i) => setSortType(i)} />
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>
