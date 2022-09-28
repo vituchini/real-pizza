bff24f8f8bc9cfe73ea86c1d082e9707ea44da90
vituchini <69416374+vituchini@users.noreply.github.com>
2022-09-28 18:56:07 +0300

    Realize cart item (#15)

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSortType } from '../redux/slices/filterSlice'
 
@@ -12,8 +12,9 @@ export const list = [
 ]
 
 function Sort() {
-  const sort = useSelector((state) => state.filter.sort)
   const dispatch = useDispatch()
+  const sort = useSelector((state) => state.filter.sort)
+  const sortRef = useRef()
 
   const [open, setOpen] = useState(false)
 
@@ -22,8 +23,20 @@ function Sort() {
     setOpen(false)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.path.includes(sortRef.current)) {
+        setOpen(false)
+      }
+    }
+
+    document.body.addEventListener('click', handleClickOutside)
+
+    return () => document.body.removeEventListener('click', handleClickOutside)
+  }, [])
+
   return (
-    <div className='sort'>
+    <div ref={sortRef} className='sort'>
       <div className='sort__label'>
         <svg
           style={{ transform: open && 'rotate(180deg)' }}
