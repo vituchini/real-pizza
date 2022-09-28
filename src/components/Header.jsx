39932a17e2ca12e0afb8c04e92db07985eff38f5
vituchini <viktor.petrovych.work@gmail.com>
2022-09-28 18:55:45 +0300

    Realize cart item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 import logo from '../assets/img/pizza-logo.svg'
 import Search from './Search'
 
 function Header() {
+  const { totalPrice, items } = useSelector((state) => state.cart)
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0)
+
   return (
     <div className='header'>
       <div className='container'>
@@ -19,7 +24,7 @@ function Header() {
         <div className='header__cart'>
           <Link to='/cart'>
             <a href='/cart.html' className='button button--cart'>
-              <span>520 ₽</span>
+              <span>{totalPrice} ₽</span>
               <div className='button__delimiter'></div>
               <svg
                 width='18'
@@ -50,7 +55,7 @@ function Header() {
                   strokeLinejoin='round'
                 />
               </svg>
-              <span>3</span>
+              <span>{totalCount}</span>
             </a>
           </Link>
         </div>
